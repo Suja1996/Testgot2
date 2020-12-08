e8aa34d548f472fbce4a342a4138d72abf57b0c4
Sujatha <sujathasaravanan1512@gmail.com>
2020-12-08 14:00:40 +0530

    created the platform

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+platform = new Ground(150,300,300,170);
     bird = new Bird(100,100);
 
 }
@@ -48,7 +48,7 @@ function draw(){
     ground.display();
     pig1.display();
     log1.display();
-
+platform.display()
     box3.display();
     box4.display();
     pig3.display();
